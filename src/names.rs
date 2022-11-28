317526ddca16e1f8d75f8927f6fc9e016d35ba9d
hectorLop <lopez.almazan.hector@gmail.com>
2022-11-28 20:43:54 +0100

    implemented read_versions_data method

--- src/names.rs
+++ src/names.rs
@@ -19,4 +19,3 @@ struct Version {
     tags: Vec<String>,
 }
 
-
--- src/versions.rs
+++ src/versions.rs
@@ -3,9 +3,39 @@ use crate::names::Names;
 use serde_derive::{Deserialize, Serialize};
 
 
+pub trait Data {}
+
+
 #[derive(Deserialize, Serialize, Debug)]
 pub struct VersionData {
     tags_data: Tags,
     names_data: Names,
 }
 
+#[derive(Deserialize, Serialize, Debug)]
+enum FileType {
+    TagFile(Tags),
+    NameFile(Names),
+}
+
+fn read_versions_data(folder: String) -> VersionData {
+    let tags_filepath = folder.clone() + "tags.json";
+    let names_filepath = folder + "names.json";
+
+    let tags_data = {
+        let str_data = std::fs::read_to_string(&tags_filepath).unwrap();
+
+        serde_json::from_str::<Tags>(&str_data).unwrap()
+    };
+    
+    let names_data = {
+        let str_data = std::fs::read_to_string(&names_filepath).unwrap();
+
+        serde_json::from_str::<Names>(&str_data).unwrap()
+    };
+
+    VersionData {
+        tags_data: tags_data,
+        names_data: names_data,
+    }
+}
